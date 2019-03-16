003000e2148b4ad0165a3d47319189484832737b
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-16 21:56:44 +1000

    Add files via upload

--- hdrtoning.cpp
+++ hdrtoning.cpp
@@ -13,17 +13,17 @@ signed class hdrtoning();
 int 2003();
 signed int 2003();
 {
- //Adobe Photoshop CC optimized filter hdr toning 
- //Adobe Photoshop CC filter hdr toning fixed quality
- //Adobe Photoshop CC filter hdr toning save quality
- //Adobe Photoshop CC filter hdr toning new techelogy
+ //Adobe Photoshop CC optimized correction hdr toning 
+ //Adobe Photoshop CC correction hdr toning fixed quality
+ //Adobe Photoshop CC correction hdr toning save quality
+ //Adobe Photoshop CC correction hdr toning new techelogy
  //Adobe Photoshop CC version 2019
  //Adobe Photoshop CC version 20.0x
- //Adobe Photoshop CC filter hdr toning fixed blur
- //Adobe Photoshop CC filter hdr toning fixed defects
- //Adobe Photoshop CC filter hdr toning fixed artifacts
- //Adobe Photoshop CC filter hdr toning fixed sharpen
- //Adobe Photoshop CC filter hdr toning fixed lighting
+ //Adobe Photoshop CC correction hdr toning fixed blur
+ //Adobe Photoshop CC correction hdr toning fixed defects
+ //Adobe Photoshop CC correction hdr toning fixed artifacts
+ //Adobe Photoshop CC correction hdr toning fixed sharpen
+ //Adobe Photoshop CC correction hdr toning fixed lighting
 }
 
 
