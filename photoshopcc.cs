0c9d1c0f1276bbb60e888b61428e367f9ff3218a
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-20 20:12:05 +1000

    Add files via upload

---
+++ photoshopcc.cs
@@ -0,0 +1,10 @@
+class photoshopcc(class.set);
+public class photoshopcc(class.set)
+request.AddParam(set.params("adobe_photoshop_cc_framerate" 144);
+request.AddParam(set.params("adobe_photoshop_cc_version" 20.0.4);
+request.AddParam(set.params("adobe_photoshop_cc_ping" 0ms);
+request.AddParam(set.params("adobe_photoshop_cc_correction_tab_framerate" 144);
+request.AddParam(set.params("adobe_photoshop_cc_filter_tab_framerate" 144);
+request.AddParam(set.params("adobe_photoshop_cc_hight_quality_photographies");
+request.AddParam(set.params("adobe_photoshop_cc_performance" 100%);
+
