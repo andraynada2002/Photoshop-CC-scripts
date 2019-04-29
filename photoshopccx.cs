45066f90b2667923930cce277bfa85db7f719796
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-29 18:33:34 +1000

    Add files via upload

---
+++ photoshopccx.cs
@@ -0,0 +1,62 @@
+public class photoshopcc;
+private class photoshopcc;
+class photoshopcc;
+
+//Adobe Photoshop CC random version
+//Adobe Photoshop CC version 2019
+//Adobe Photoshop CC random yer version
+//Adobe Photoshop CC auto detected version
+//Adobe Photoshop CC auto detected yer version
+//Adobe Photoshop CC executable file = @"photoshop.exe"
+//Adobe Photoshop CC fixed curve
+//Adobe Photoshop CC fixed levels
+//Adobe Photoshop CC fixed exposure
+//Adobe Photoshop CC fixed HDR toning
+//Adobe Photoshop CC optimized performance
+//Adobe Photoshop CC HDR toning detached
+//Adobe Photoshop CC HDR toning attached
+//Adobe Photoshop CC detached
+//Adobe Photoshop CC attached
+//Adobe Photoshop CC levels detached
+//Adobe Photoshop CC levels attached
+//Adobe Photoshop CC curves detached
+//Adobe Photoshop CC curves attached
+//Adobe Photoshop CC performance detached
+//Adobe Photoshop CC performance attached
+//Adobe Photoshop CC RGB fixed
+//Adobe Photoshop CC RGB detached
+//Adobe Photoshop CC RGB attached
+//Adobe Photoshop CC executable detached
+//Adobe Photoshop CC executable attached
+//Adobe Photoshop CC image manipulation fixed
+//Adobe Photoshop CC image manipulation detached
+//Adobe Photoshop CC image manipulation attached
+//Adobe Photoshop CC image correction fixed
+//Adobe Photoshop CC image correction detached
+//Adobe Photoshop CC image correction attached
+//Adobe Photoshop CC fixed RGB quality
+//Adobe Photoshop CC RGB quality detached
+//Adobe Photoshop CC RGB quality attached
+namespace photoshopcc;
+{
+	public void photoshopcc;
+	private void photoshopcc;
+	public int 294;
+	private int 290;
+	public protected photoshopcc;
+	private protected photoshopcc;
+	public get photoshopcc;
+	private get photoshopcc;
+	public set photoshopcc;
+	private set photoshopcc;
+	public add photoshopcc;
+	private add photoshopcc;
+}
+
+
+
+
+
+
+
+
